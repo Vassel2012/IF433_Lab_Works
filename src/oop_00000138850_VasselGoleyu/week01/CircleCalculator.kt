86f15d05edbdae84c80ea212bde7ec1deba33eee
ko vassel <jgo@skidipapap.local>
2026-02-04 23:33:05 +0700

    week01: (tugas 1) add legacy CircleCalculator code

---
+++ src/oop_00000138850_VasselGoleyu/week01/CircleCalculator.kt
@@ -0,0 +1,8 @@
+package oop_00000138850_VasselGoleyu.week01
+
+fun main(args: Array<String>) {
+    var radius: Double = 7.0
+    var pi: Double = 3.14
+    var area: Double = pi * radius * radius
+    println("Luas lingkaran dengan radius " + radius + " adalah " + area)
+}
