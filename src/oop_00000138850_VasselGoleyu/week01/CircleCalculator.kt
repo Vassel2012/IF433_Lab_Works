29fb25e8423587418474561fe81eb73038bf7236
ko vassel <jgo@skidipapap.local>
2026-02-04 23:33:33 +0700

    week01: (tugas 1) convert checkSize to expression body function

--- src/oop_00000138850_VasselGoleyu/week01/CircleCalculator.kt
+++ src/oop_00000138850_VasselGoleyu/week01/CircleCalculator.kt
@@ -1,8 +1,11 @@
 package oop_00000138850_VasselGoleyu.week01
 
+fun checkSize(area: Double) = if (area > 100) "Besar" else "Kecil"
+
 fun main(args: Array<String>) {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
     println("Luas lingkaran dengan radius $radius adalah $area")
+    println("Kategori: ${checkSize(area)}")
 }
