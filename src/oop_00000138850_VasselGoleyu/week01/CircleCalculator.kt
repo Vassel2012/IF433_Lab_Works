302fb80f00951127abce2d751e0e899407afa513
ko vassel <jgo@skidipapap.local>
2026-02-04 23:33:53 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/oop_00000138850_VasselGoleyu/week01/CircleCalculator.kt
+++ src/oop_00000138850_VasselGoleyu/week01/CircleCalculator.kt
@@ -1,8 +1,6 @@
 package oop_00000138850_VasselGoleyu.week01
 
-fun checkSize(area: Double) = if (area > 100) "Besar" else "Kecil"
-
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
