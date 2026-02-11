8ecfdd4a02ddd89c2730e3be8dc864cffadf0614
ko vassel <jgo@skidipapap.local>
2026-02-11 15:59:40 +0700

    week02: add secondary constructor

--- src/oop_00000138850_VasselGoleyu/week02/Student.kt
+++ src/oop_00000138850_VasselGoleyu/week02/Student.kt
@@ -1,15 +1,27 @@
-package oop_00000138850_VasselGoleyu.week02
-
 package oop_001_johnthor.week02
 
-class Student(val name: String, val nim: String, var major: String) {
-    init {
-        // Validasi Sederhana: Cek panjang NIM
-        if (nim.length != 5) {
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-        }
-    }
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("-- APLIKASI PMB UNN ---")
+
+    print("Masukkan Nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukkan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+    scanner.nextLine() // Membersihkan buffer
+
+    print("Masukkan Jurusan: ")
+    val major = scanner.nextLine()
+
+    // Membuat objek Student
+    val student = Student(name, nim, major)
+
+    println("\nData Mahasiswa:")
+    println("Nama: ${student.name}")
+    println("NIM: ${student.nim}")
+    println("Jurusan: ${student.major}")
 }
