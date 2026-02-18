c97d65403657501daa8ff4eff169698726a6cab0
ko vassel <jgo@skidipapap.local>
2026-02-18 15:41:49 +0700

    week03: create basic Employee class

--- src/oop_00000138850_VasselGoleyu/week03/Employee.kt
+++ src/oop_00000138850_VasselGoleyu/week03/Employee.kt
@@ -2,7 +2,4 @@ package oop_00000138850_VasselGoleyu.week03
 
 class Employee {
     var salary: Int = 0
-        set(value) {
-            this.salary = value
-        }
 }
--- src/oop_00000138850_VasselGoleyu/week03/Main.kt
+++ src/oop_00000138850_VasselGoleyu/week03/Main.kt
@@ -1,23 +1,7 @@
 package oop_00000138850_VasselGoleyu.week03
 
-class Employee {
-
-    var salary: Int = 0
-        set(value) {
-            if (value < 0) {
-                println("Gaji tidak boleh negatif")
-            } else {
-                field = value
-            }
-        }
-
-    private var performanceRating: Int = 0
-
-    fun updatePerformance(rating: Int) {
-        if (rating in 1..5) {
-            performanceRating = rating
-        } else {
-            println("Rating harus 1 - 5")
-        }
-    }
+fun main() {
+    val employee = Employee()
+    employee.salary = 5000
+    println("Salary: ${employee.salary}")
 }
