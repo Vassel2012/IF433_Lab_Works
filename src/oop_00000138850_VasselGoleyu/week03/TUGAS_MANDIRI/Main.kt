f70ed5ed43e936872ef4c48777389176e8114516
ko vassel <jgo@skidipapap.local>
2026-02-18 15:45:44 +0700

    week03: create Weapon class with validation and tier

---
+++ src/oop_00000138850_VasselGoleyu/week03/TUGAS_MANDIRI/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000138850_VasselGoleyu.week03.TUGAS_MANDIRI
+
+val weapon = Weapon("Excalibur")
+
+weapon.damage = -10
+weapon.damage = 1200
+
+println("Weapon: ${weapon.name}")
+println("Damage: ${weapon.damage}")
+println("Tier: ${weapon.tier}")
