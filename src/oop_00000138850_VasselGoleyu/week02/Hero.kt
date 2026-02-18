c9a9b4a3769036385ba8853c1562bae274d7c95e
sellebeww <vassel.jgo@gmail.com>
2026-02-18 15:27:34 +0700

    Update diluar tugas Lab

--- src/oop_00000138850_VasselGoleyu/week02/Hero.kt
+++ src/oop_00000138850_VasselGoleyu/week02/Hero.kt
@@ -1,7 +1,5 @@
 package oop_00000138850_VasselGoleyu.week02
 
-package oop_001_johnthor.week02
-
 class Hero(
     val name: String,
     var hp: Int,
@@ -12,12 +10,10 @@ class Hero(
         println("Hero $name telah diciptakan dengan HP: $hp dan Damage: $baseDamage")
     }
 
-    // Method untuk menyerang
     fun attack(targetName: String) {
         println("\n⚔️ $name menebas $targetName!")
     }
 
-    // Method untuk menerima damage
     fun takeDamage(damage: Int) {
         hp -= damage
         println("💥 $name menerima $damage damage!")
@@ -30,12 +26,10 @@ class Hero(
         }
     }
 
-    // Method untuk mengecek apakah hero masih hidup
     fun isAlive(): Boolean {
         return hp > 0
     }
 
-    // Method untuk menampilkan status
     fun displayStatus() {
         println("\n=== Status Hero ===")
         println("Nama: $name")
