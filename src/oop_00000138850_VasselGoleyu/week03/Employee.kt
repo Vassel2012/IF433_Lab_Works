44bfcbbb92d75417193897a79a63bb4ca30b0b90
ko vassel <jgo@skidipapap.local>
2026-02-18 15:36:23 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000138850_VasselGoleyu/week03/Employee.kt
+++ src/oop_00000138850_VasselGoleyu/week03/Employee.kt
@@ -2,4 +2,7 @@ package oop_00000138850_VasselGoleyu.week03
 
 class Employee {
     var salary: Int = 0
+        set(value) {
+            this.salary = value
+        }
 }
---
+++ src/oop_00000138850_VasselGoleyu/week03/Main.kt
@@ -0,0 +1,6 @@
+package oop_00000138850_VasselGoleyu.week03
+
+fun main() {
+    val e = Employee()
+    e.salary = 5000
+}
