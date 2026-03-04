94073acc51d11dbbbb1cc36c9afbb783d0a34c7a
ko vassel <jgo@skidipapap.local>
2026-03-04 15:30:08 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000138850_VasselGoleyu/Week05/Main.kt
+++ src/oop_00000138850_VasselGoleyu/Week05/Main.kt
@@ -29,4 +29,23 @@ fun main() {
     println("Luas Persegi: ${math.hitungLuas(4)}")
     println("Luas Persegi Panjang: ${math.hitungLuas(4, 5)}")
     println("Luas Lingkaran: ${math.hitungLuas(7.0)}")
+
+    println("\n===== PAYMENT SYSTEM =====")
+
+    val wallet = EWallet("Vassel", 50000.0)
+    val card = CreditCard("Vassel", 100000.0)
+
+    val payments: List<PaymentMethod> = listOf(wallet, card)
+
+    for (payment in payments) {
+
+        payment.processPayment(75000.0)
+
+        if (payment is EWallet) {
+            payment.topUp(50000.0)
+            payment.processPayment(75000.0)
+        }
+
+        println("--------------------")
+    }
 }
