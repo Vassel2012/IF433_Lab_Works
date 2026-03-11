c1a6fe8b4af3725c846e71e8aa0bc9f738987c1c
ko vassel <jgo@skidipapap.local>
2026-03-11 18:05:55 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000138850_VasselGoleyu/Week06/GadgetInterfaces.kt
@@ -0,0 +1,17 @@
+package oop_00000138850_VasselGoleyu.Week06
+
+interface Camera {
+
+    fun takePhoto() {
+        println("Mengambil foto dengan kamera")
+    }
+
+}
+
+interface Phone {
+
+    fun takePhoto() {
+        println("Mengambil foto dengan kamera smartphone")
+    }
+
+}
