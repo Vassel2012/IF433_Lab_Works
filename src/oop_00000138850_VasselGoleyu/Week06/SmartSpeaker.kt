88669a1279efaee470bdecab9aa2626feb9ca076
ko vassel <jgo@skidipapap.local>
2026-03-11 18:10:19 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_00000138850_VasselGoleyu/Week06/SmartSpeaker.kt
@@ -0,0 +1,20 @@
+package oop_00000138850_VasselGoleyu.Week06
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("Speaker $name dinyalakan")
+    }
+
+    override fun turnOff() {
+        println("Speaker $name dimatikan")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+
+}
