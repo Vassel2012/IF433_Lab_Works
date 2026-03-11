355a15f010854d564251528bf9f949f4559ae590
ko vassel <jgo@skidipapap.local>
2026-03-11 18:09:20 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_00000138850_VasselGoleyu/Week06/SmartInterfaces.kt
@@ -0,0 +1,25 @@
+package oop_00000138850_VasselGoleyu.Week06
+
+interface SmartDevice {
+
+    val id: String
+    val name: String
+
+}
+
+interface Switchable {
+
+    fun turnOn()
+    fun turnOff()
+
+}
+
+interface Recordable {
+
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+
+}
