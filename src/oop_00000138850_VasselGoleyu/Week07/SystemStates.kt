2fc2c39d4333165381b828b449b8ddebcf64fa11
ko vassel <jgo@skidipapap.local>
2026-03-25 15:56:55 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000138850_VasselGoleyu/Week07/SystemStates.kt
+++ src/oop_00000138850_VasselGoleyu/Week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_00000138850_VasselGoleyu.Week07
 enum class AppState {
     LOADING, SUCCESS, ERROR
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
