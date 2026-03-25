fd3055b562a3c133b8a9fcaa4aa364421ebbb6c4
ko vassel <jgo@skidipapap.local>
2026-03-25 15:52:25 +0700

    week07: (trial) trigger private constructor error on NetworkClient

---
+++ src/oop_00000138850_VasselGoleyu/Week07/Main.kt
@@ -0,0 +1,5 @@
+package oop_00000138850_VasselGoleyu.Week07
+
+fun main() {
+    val client = NetworkClient("https://api.umn.ac.id") // ERROR
+}
---
+++ src/oop_00000138850_VasselGoleyu/Week07/NetworkClient.kt
@@ -0,0 +1,3 @@
+package oop_00000138850_VasselGoleyu.Week07
+
+class NetworkClient private constructor(val baseUrl: String)
