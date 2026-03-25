f8f88aeb003972f263c78d12cd006de7d3526f15
ko vassel <jgo@skidipapap.local>
2026-03-25 16:00:33 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000138850_VasselGoleyu/Week07/Main.kt
+++ src/oop_00000138850_VasselGoleyu/Week07/Main.kt
@@ -3,4 +3,9 @@ package oop_00000138850_VasselGoleyu.Week07
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println(ItemRarity.LEGENDARY.dropChance)
+
+    val weapon = Weapon.forgeStarterSword()
+    println("${weapon.item.name} - ${weapon.item.damage}")
 }
