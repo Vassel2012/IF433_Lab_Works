917e71d849c642b747b0be74e5265a02a4f62854
ko vassel <jgo@skidipapap.local>
2026-03-25 15:57:12 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_00000138850_VasselGoleyu/Week07/Main.kt
+++ src/oop_00000138850_VasselGoleyu/Week07/Main.kt
@@ -1,26 +1,10 @@
 package oop_00000138850_VasselGoleyu.Week07
 
 fun main() {
-    DatabaseManager.connect()
+    val response: ApiResponse = ApiResponse.Success("OK")
 
-    val client = NetworkClient.create("https://api.umn.ac.id")
-    println(client.baseUrl)
-
-    val u1 = User("A", 20)
-    val u2 = User("A", 20)
-
-    println(u1)
-    println(u1 == u2)
-
-    val d1 = UserData("A", 20)
-    val d2 = UserData("A", 20)
-
-    println(d1)
-    println(d1 == d2)
-
-    val d3 = d1.copy(age = 25)
-    println(d3)
-
-    val (name, age) = d3
-    println("$name $age")
+    val result = when (response) {
+        is ApiResponse.Success -> "Success"
+        is ApiResponse.Error -> "Error"
+    }
 }
