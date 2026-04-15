81a0a79a16fea4be1ceb8f33c91ba421cdc0cab5
ko vassel <jgo@skidipapap.local>
2026-04-15 15:43:14 +0700

    week08: implement smart cast in function

---
+++ src/oop_00000138850_VasselGoleyu/Week08/NotificationService.kt
@@ -0,0 +1,20 @@
+package oop_00000138850_VasselGoleyu.Week08
+
+class NotificationService {
+
+    fun sendEmail(emailAddress: String) {
+        println("Mengirim email ke: $emailAddress")
+    }
+
+    fun processUser(user: UserProfile) {
+        // sendEmail(user.email) // INI AKAN ERROR: Type mismatch
+
+        if (user.email != null) {
+            // SUCCESS via Smart Cast: compiler tahu 'user.email'
+            // pasti tidak null di blok ini
+            sendEmail(emailAddress = user.email)
+        } else {
+            println("User ${user.name} tidak memiliki email.")
+        }
+    }
+}
