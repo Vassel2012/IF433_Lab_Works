a08d046d152ed13d5b21856426aa57593a3a33f9
ko vassel <jgo@skidipapap.local>
2026-04-15 15:52:58 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000138850_VasselGoleyu/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000138850_VasselGoleyu.Week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
