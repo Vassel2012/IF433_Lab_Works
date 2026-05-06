1aea17b83ae44cb95b42d39efebe908d793b70a5
ko vassel <jgo@skidipapap.local>
2026-05-06 14:53:51 +0700

    week11: (task) implement device search with let

--- src/oop_00000138850_VasselGoleyu/week11/SmartHomeDashboard.kt
+++ src/oop_00000138850_VasselGoleyu/week11/SmartHomeDashboard.kt
@@ -34,4 +34,10 @@ fun main() {
 
     homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
     println("Pet feeder ditambahkan.")
+
+    println("\n=== PENCARIAN PERANGKAT ===")
+    val searchResult = homeDevices.find { it.category == "Camera" }
+    searchResult?.let {
+        println(it.diagnose())
+    }
 }
