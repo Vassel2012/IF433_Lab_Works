d56bd08d9c19dfb329f3547328a4aa5891c8c0a5
ko vassel <jgo@skidipapap.local>
2026-05-06 14:52:35 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_00000138850_VasselGoleyu/week11/SmartHomeDashboard.kt
@@ -0,0 +1,10 @@
+package oop_00000138850_VasselGoleyu.week11
+
+fun main() {
+    println("╔══════════════════════════════════════════╗")
+    println("║     SMART HOME DASHBOARD v1.0            ║")
+    println("╚══════════════════════════════════════════╝")
+
+    val homeDevices = mutableListOf<SmartDevice>()
+    println("Smart home device list berhasil diinisialisasi.")
+}
