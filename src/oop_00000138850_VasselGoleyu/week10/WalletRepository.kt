d4c365c67b0cf2ea1738d8098f0a4be4513bc0af
ko vassel <jgo@skidipapap.local>
2026-05-06 14:38:31 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000138850_VasselGoleyu/week10/WalletRepository.kt
+++ src/oop_00000138850_VasselGoleyu/week10/WalletRepository.kt
@@ -1,6 +1,6 @@
 package oop_00000138850_VasselGoleyu.Week10
 
-class WalletRepository<T> {
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
 
     fun add(item: T) {
@@ -9,5 +9,15 @@ class WalletRepository<T> {
 
     fun getAll(): List<T> {
         return items.toList()
+    }
+
+    fun getCount(): Int {
+        return items.size
+    }
+
+    fun search(keyword: String): List<T> {
+        return items.filter { item ->
+            item.toString().contains(keyword, ignoreCase = true)
+        }
     }
 }
