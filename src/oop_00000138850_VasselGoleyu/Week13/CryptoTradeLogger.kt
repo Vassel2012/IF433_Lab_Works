67e6b443beead4806b6503291ebe3e89dc15e32f
ko vassel <jgo@skidipapap.local>
2026-05-19 22:02:00 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000138850_VasselGoleyu/Week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_00000138850_VasselGoleyu.Week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
